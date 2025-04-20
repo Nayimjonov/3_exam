62258b96b49d6920b03c22f0b7b33841be1449fe
Samandar <samandarnayimjonov@gmail.com>
2025-04-20 18:32:29 +0500

    image model full done

--- images/models.py
+++ images/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from listings.models import Listing
 
-# Create your models here.
+
+class Image(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='car_images/')
+    is_primary = models.BooleanField(default=False)
+    order = models.PositiveSmallIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"Image for {self.listing.title} (Primary: {self.is_primary})"
