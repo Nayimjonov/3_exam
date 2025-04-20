ef415577409e0ca3fb4ee1c7299563e85b07fe8e
Samandar <samandarnayimjonov@gmail.com>
2025-04-20 18:47:13 +0500

    adminka done

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('reviewer', 'reviewed_user', 'listing', 'rating', 'created_at')
+    search_fields = ('reviewer__username', 'reviewed_user__username', 'listing__title', 'comment')
+    list_filter = ('rating', 'created_at')
+    readonly_fields = ('created_at',)
+
+    list_editable = ('rating',)
