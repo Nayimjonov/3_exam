d64c05868e4b583369998449e3d5b47e26c14749
Samandar <samandarnayimjonov@gmail.com>
2025-04-20 18:47:42 +0500

    adminka done

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'user_type', 'phone', 'location', 'rating', 'created_at')
+    search_fields = ('user__username', 'phone', 'location')
+    list_filter = ('user_type', 'created_at')
+    readonly_fields = ('created_at',)
+
+    list_editable = ('user_type', 'rating')
