5d3f06eb56006e010ea6432ac95d483038394dc6
Samandar <samandarnayimjonov@gmail.com>
2025-04-20 18:13:59 +0500

    users models full done

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    CHOICES_USER_TYPE = [
+        ('oddiy', 'Oddiy'),
+        ('dile', 'Diler')
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    user_type = models.CharField(max_length=5, choices=CHOICES_USER_TYPE, default=CHOICES_USER_TYPE[0][0])
+    phone = models.CharField(max_length=13)
+    avatar = models.ImageField(upload_to='users_avatars/')
+    location = models.CharField(max_length=255)
+    rating = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
