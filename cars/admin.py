fe210b9e14d1cb943c575f448ca2b2ca834249d4
Samandar <samandarnayimjonov@gmail.com>
2025-04-20 18:43:59 +0500

    adminka done

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Car
 
-# Register your models here.
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('make', 'model', 'year', 'fuel_type', 'transmission', 'color', 'mileage', 'engine_size', 'power', 'drive_type', 'vin', 'created_at', 'updated_at')
+    search_fields = ('make__name', 'model__name', 'vin')
+    list_filter = ('fuel_type', 'transmission', 'drive_type', 'year')
+    list_editable = ('fuel_type', 'transmission', 'drive_type')
+
+    filter_horizontal = ('features',)
+
+admin.site.register(Car, CarAdmin)
--- dealers/admin.py
+++ dealers/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Dealer
 
-# Register your models here.
+@admin.register(Dealer)
+class DealerAdmin(admin.ModelAdmin):
+    list_display = ('user', 'company_name', 'rating', 'is_verified', 'created_at')
+    search_fields = ('user__username', 'company_name')
+    list_filter = ('is_verified', 'rating')
+    list_editable = ('is_verified', 'rating')
+    readonly_fields = ('logo',)
+
+    fieldsets = (
+        (None, {
+            'fields': ('user', 'company_name', 'description', 'logo', 'website', 'address', 'is_verified', 'rating')
+        }),
+        ('Timestamp', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',)
+        }),
+    )
+
+    def logo_preview(self, obj):
+        if obj.logo:
+            return f'<img src="{obj.logo.url}" width="100" height="100"/>'
+        return "No Logo"
+    logo_preview.allow_tags = True
+    logo_preview.short_description = 'Logo Preview'
