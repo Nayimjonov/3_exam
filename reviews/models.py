3de07286d8b9bba9d40c099f1bff5a49da07e3fd
Samandar <samandarnayimjonov@gmail.com>
2025-04-20 18:34:51 +0500

    review model done

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from listings.models import Listing  # Импорт модели Listing
 
-# Create your models here.
+class Review(models.Model):
+    reviewer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews_written')
+    reviewed_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews_received')
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(choices=[(i, i) for i in range(1, 6)])  # Рейтинг от 1 до 5
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"Review by {self.reviewer.username} for {self.reviewed_user.username} on {self.listing.title}"
