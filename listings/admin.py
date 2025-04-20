4f2f27b642b1ec3f3f7a66c5f5524b49028a872c
Samandar <samandarnayimjonov@gmail.com>
2025-04-20 18:45:45 +0500

    adminka done

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Listing
 
-# Register your models here.
+@admin.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('title', 'seller', 'price', 'currency', 'condition', 'is_active', 'is_featured')
+    search_fields = ('title', 'seller__username', 'car__make__name', 'car__model__name')
+    list_filter = ('condition', 'currency', 'is_active', 'is_featured')
+    list_editable = ('is_active', 'is_featured')
+    readonly_fields = ('created_at', 'updated_at')
+
+    filter_horizontal = ('car',)
