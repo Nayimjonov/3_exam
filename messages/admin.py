bf1848cd059375289e96818d17c3f30495291bd3
Samandar <samandarnayimjonov@gmail.com>
2025-04-20 18:46:26 +0500

    adminka done

--- messages/admin.py
+++ messages/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Message
 
-# Register your models here.
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('sender', 'receiver', 'listing', 'is_read', 'created_at')
+    search_fields = ('sender__username', 'receiver__username', 'listing__title', 'content')
+    list_filter = ('is_read', 'created_at')
+    readonly_fields = ('created_at',)
+
+    list_editable = ('is_read',)
