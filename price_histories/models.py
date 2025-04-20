4034e9893f56ec872003ce48c6e8b0c1f5f2629c
Samandar <samandarnayimjonov@gmail.com>
2025-04-20 18:36:16 +0500

    price_history model done

--- price_histories/models.py
+++ price_histories/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from listings.models import Listing
 
-# Create your models here.
+
+class PriceHistory(models.Model):
+    CURRENCY_CHOICES = [
+        ('UZS', 'So‘m'),
+        ('USD', 'AQSh dollari'),
+    ]
+
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='price_history')
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Price history for {self.listing.title}"
+
+
