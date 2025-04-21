b3871c5ad873387fa4f7979a6fa6c5210200c93f
Samandar <samandarnayimjonov@gmail.com>
2025-04-21 09:44:49 +0500

    updated

--- dealers/serializers.py
+++ dealers/serializers.py
@@ -46,6 +46,36 @@ class DealerCreateSerializer(serializers.ModelSerializer):
         user = self.context['request'].user
         return Dealer.objects.create(user=user, **validated_data)
 
+# dealer/<id>/listing
+class CarSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    make = serializers.CharField(max_length=100)
+    model = serializers.CharField(max_length=100)
+    year = serializers.IntegerField()
 
+class ListingSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=200)
+    price = serializers.DecimalField(max_digits=10, decimal_places=2)
+    currency = serializers.CharField(max_length=10)
+    location = serializers.CharField(max_length=100)
+    condition = serializers.CharField(max_length=50)
+    is_negotiable = serializers.BooleanField()
+    is_active = serializers.BooleanField()
+    is_featured = serializers.BooleanField()
+    views_count = serializers.IntegerField()
+    created_at = serializers.DateTimeField()
+    car = CarSerializer()
+    primary_image = serializers.ImageField()
 
+class DealerListingSerializer(serializers.Serializer):
+    user = serializers.StringRelatedField()
+    company_name = serializers.CharField(max_length=200)
+    description = serializers.CharField()
+    logo = serializers.ImageField()
+    website = serializers.URLField()
+    address = serializers.CharField()
+    is_verified = serializers.BooleanField()
+    rating = serializers.FloatField()
+    listings = ListingSerializer(many=True)
 
