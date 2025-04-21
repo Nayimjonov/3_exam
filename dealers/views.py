e71e59e076795d8e0c961b2d727b5f6cf9e891ca
Samandar <samandarnayimjonov@gmail.com>
2025-04-21 09:09:19 +0500

    pagination class qushildi

--- dealers/views.py
+++ dealers/views.py
@@ -1,3 +1,8 @@
 from rest_framework import generics
 from .models import Dealer
 from .serializers import DealerSerializer
+
+
+class DealerListCreateVie(generics.ListCreateAPIView):
+    queryset = Dealer.objects.all()
+    serializer_class = DealerSerializer
