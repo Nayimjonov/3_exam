f8c000ae50fc86171a425e36674a17448605af68
Samandar <samandarnayimjonov@gmail.com>
2025-04-21 17:36:18 +0500

    updated

--- vehicles/urls.py
+++ vehicles/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('makes/', views.MakeListView.as_view(), name='make-list'),
     path('makes/<int:make_id>/models/', views.ModelListByMakeView.as_view(), name='model-list-by-make'),
+    path('body-types/', views.BodyTypeListView.as_view(), name='body-types'),
+    path('features/', views.FeatureListView.as_view(), name='features'),
 
 ]
