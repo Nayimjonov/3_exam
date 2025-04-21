b9e21073ccf8440a6d37c2e1dc5a443aae7705f0
Samandar <samandarnayimjonov@gmail.com>
2025-04-21 08:38:29 +0500

    dealer serializer done

--- dealers/serializers.py
+++ dealers/serializers.py
@@ -1,1 +1,34 @@
 from rest_framework import serializers
+from .models import Dealer
+
+
+class DealerUserSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    username = serializers.CharField(read_only=True)
+    first_name = serializers.CharField(read_only=True)
+    last_name = serializers.CharField(read_only=True)
+
+
+
+class DealerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Dealer
+        fields = (
+            'id',
+            'user',
+            'company_name',
+            'description',
+            'logo',
+            'website',
+            'address',
+            'is_verified',
+            'rating',
+            'created_at',
+            'updated_at'
+        )
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['user'] = DealerUserSerializer(instance.user).data
+        return representation
+
