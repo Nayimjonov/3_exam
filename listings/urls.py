18b6746e5514873c77da259bd92cd92bc8890847
Samandar <samandarnayimjonov@gmail.com>
2025-04-22 17:41:29 +0500

    updated

--- listings/urls.py
+++ listings/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('listings/', views.ListingListCreateView.as_view(), name='listing-list'),
+    path('listings/<int:pk>/', views.ListingRetrieveUpdateDestroyView.as_view(), name='listing-detail'),
 
 ]
