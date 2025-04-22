0c0b9441464c8fb8737dcb121bb6247dcb691e02
Samandar <samandarnayimjonov@gmail.com>
2025-04-22 16:48:43 +0500

    updated

--- listings/views.py
+++ listings/views.py
@@ -1,1 +1,8 @@
 from  rest_framework import generics
+from .models import Listing
+from .serializers import ListingSerializer
+
+
+class ListingListXCreateView(generics.ListCreateAPIView):
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
