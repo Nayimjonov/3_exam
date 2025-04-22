babb9cbf2f74313993eacf33102b1b9889bf1145
Samandar <samandarnayimjonov@gmail.com>
2025-04-22 16:44:21 +0500

    updated

--- listings/serializers.py
+++ listings/serializers.py
@@ -2,6 +2,12 @@ from rest_framework import serializers
 from .models import Listing
 
 
+class ListingCarSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    make = serializers.CharField(read_only=True)
+    model = serializers.CharField(read_only=True)
+    year = serializers.DateField(read_only=True)
+    body_type = serializers.CharField(read_only=True)
 
 
 class ListingSerializer(serializers.ModelSerializer):
