e59b7d46bc44f2d37ec2c7d6847c638603019a3e
Samandar <samandarnayimjonov@gmail.com>
2025-04-22 16:42:44 +0500

    updated

--- listings/serializers.py
+++ listings/serializers.py
@@ -1,1 +1,28 @@
 from rest_framework import serializers
+from .models import Listing
+
+
+
+
+class ListingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Listing
+        fields = (
+            'id',
+            'title',
+            'description',
+            'price',
+            'currency',
+            'location',
+            'condition',
+            'is_negotiable',
+            'is_active',
+            'views_count',
+            'created_at',
+            'updated_at',
+            'expires_at',
+            'car',
+            'seller',
+            'primary_image',
+            'images_count'
+        )
--- vehicles/views.py
+++ vehicles/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.permissions import AllowAny
 from .models import Make, Model, BodyType, Feature
 from .serializers import MakeSerializer, ModelSerializer, BodyTypeSerializer, FeatureSerializer
 from core.paginations import MakePagination, BodyTypePagination, FeaturePagination
@@ -8,11 +9,13 @@ class MakeListView(generics.ListAPIView):
     queryset = Make.objects.all()
     serializer_class = MakeSerializer
     pagination_class = MakePagination
+    permission_classes = [AllowAny]
 
 
 class ModelListByMakeView(generics.ListAPIView):
     serializer_class = ModelSerializer
     pagination_class = MakePagination
+    permission_classes = [AllowAny]
 
     def get_queryset(self):
         make_id = self.kwargs.get('make_id')
@@ -20,11 +23,15 @@ class ModelListByMakeView(generics.ListAPIView):
 
 
 class BodyTypeListView(generics.ListAPIView):
-    queryset = BodyType
+    queryset = BodyType.objects.all()
     serializer_class = BodyTypeSerializer
     pagination_class = BodyTypePagination
+    permission_classes = [AllowAny]
+
 
 class FeatureListView(generics.ListAPIView):
-    queryset = Feature
+    queryset = Feature.objects.all()
     serializer_class = FeatureSerializer
     pagination_class = FeaturePagination
+    permission_classes = [AllowAny]
+
