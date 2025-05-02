bace6a2248d1f62568a76c7e1e1701f077378638
Samandar <samandarnayimjonov@gmail.com>
2025-05-02 09:49:57 +0500

    updated

--- user_messages/serializers.py
+++ user_messages/serializers.py
@@ -1,1 +1,29 @@
 from rest_framework import serializers
+
+
+class MessageSenderSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    username = serializers.CharField(read_only=True)
+    first_name = serializers.CharField(read_only=True)
+    last_name = serializers.CharField(read_only=True)
+
+class MessageReceiverSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    username = serializers.CharField(read_only=True)
+    first_name = serializers.CharField(read_only=True)
+    last_name = serializers.CharField(read_only=True)
+
+class MessageListingSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(read_only=True)
+
+
+class MessageSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    sender = MessageSenderSerializer(read_only=True)
+    receiver = MessageReceiverSerializer(read_only=True)
+    listing = MessageListingSerializer(read_only=True)
+    content = serializers.CharField(read_only=True)
+    is_read = serializers.BooleanField(read_only=True)
+
+
